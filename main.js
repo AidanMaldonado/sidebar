ce98172ab560cba1d321fd026a4b50f0d54a4dc7
Aidan Maldonado <aidanm1198@gmail.com>
2020-09-13 00:59:22 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("special-sidebar").style.width = "25rem";
+  document.getElementById("container").style.marginLeft = "25rem";
+}
+
+function closeNav() {
+  document.getElementById("special-sidebar").style.width = "0";
+  document.getElementById("container").style.marginLeft = "0";
+}
